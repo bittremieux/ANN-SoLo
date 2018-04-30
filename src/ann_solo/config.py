95a51c68f648f7cc9a92ba709f087259648055b0
Wout Bittremieux <wout@bittremieux.be>
2018-04-30 15:18:04 -0700

    Set config descriptions and default values

--- src/ann_solo/config.py
+++ src/ann_solo/config.py
@@ -1,6 +1,15 @@
-import os
+import argparse
 
 import configargparse
+
+
+class NewlineTextHelpFormatter(argparse.HelpFormatter):
+
+    def _fill_text(self, text, width, indent):
+        return '\n'.join(argparse._textwrap.fill(
+                line, width, initial_indent=indent, subsequent_indent=indent,
+                replace_whitespace=False).strip()
+                         for line in text.splitlines(keepends=True))
 
 
 class Config:
@@ -18,22 +27,32 @@ class Config:
         """
 
         self._parser = configargparse.ArgParser(
-                description='Approximate nearest neighbor spectral library '
-                            'searching',
-                default_config_files=[
-                    './config.ini', os.path.join(
-                            os.path.dirname(os.path.realpath(__file__)),
-                            'config.ini')])
+                description='ANN-SoLo: Approximate nearest neighbor spectral '
+                            'library searching\n'
+                            '================================================='
+                            '================\n\n'
+                            'Bittremieux et al. Fast open modification '
+                            'spectral library searching through approximate '
+                            'nearest neighbor indexing. TODO: publication '
+                            'information.\n\n'
+                            'Official code website: '
+                            'https://github.com/bittremieux/ANN-SoLo\n\n',
+                default_config_files=['config.ini'],
+                formatter_class=NewlineTextHelpFormatter)
+
+        self._parser.add_argument(
+                '--config', default='config.ini',
+                help='config file (default: %(default)s)')
 
         # IO
         self._parser.add_argument(
                 'spectral_library_filename', help='spectral library file')
         self._parser.add_argument(
-                'query_filename', help='mgf file to identify')
+                'query_filename', help='query mgf file')
         self._parser.add_argument(
                 'out_filename',
-                help='the name of the mzTab output file containing the '
-                     'identifications')
+                help='name of the mzTab output file containing the search '
+                     'results')
 
         # PREPROCESSING
         # spectral library resolution to round mass values
@@ -62,7 +81,7 @@ class Config:
 
         # minimum fragment peak intensity to filter out noise peaks
         self._parser.add_argument(
-                '--min_intensity', default=0.001, type=float,
+                '--min_intensity', default=0.01, type=float,
                 help='remove peaks with a lower intensity relative to the '
                      'maximum intensity (default: %(default)s)')
 
@@ -83,7 +102,7 @@ class Config:
 
         # manner in which to scale the peak intensities
         self._parser.add_argument(
-                '--scaling', default='sqrt', type=str,
+                '--scaling', default='rank', type=str,
                 choices=['sqrt', 'rank'],
                 help='to reduce the influence of very intense peaks, scale the'
                      ' peaks by their square root or by their rank '
@@ -93,14 +112,16 @@ class Config:
         # maximum SSM precursor mass tolerance
         self._parser.add_argument(
                 '--precursor_tolerance_mass', type=float, required=True,
-                help='precursor mass tolerance')
+                help='precursor mass tolerance (small window for the first '
+                     'level of the cascade search)')
         self._parser.add_argument(
                 '--precursor_tolerance_mode', type=str,
                 choices=['Da', 'ppm'], required=True,
                 help='precursor mass tolerance unit (options: %(choices)s)')
         self._parser.add_argument(
                 '--precursor_tolerance_mass_open', type=float,
-                help='precursor mass tolerance')
+                help='precursor mass tolerance (wide window for the second '
+                     'level of the cascade search')
         self._parser.add_argument(
                 '--precursor_tolerance_mode_open', type=str,
                 choices=['Da', 'ppm'],
@@ -114,34 +135,36 @@ class Config:
         # shifted dot product
         self._parser.add_argument(
                 '--allow_peak_shifts', action='store_true',
-                help='allow shifted peaks according to the  precursor mass '
-                     'difference to  accommodate for PTMs while calculating '
-                     'the dot product match score')
+                help='use the shifted dot product instead of the standard dot '
+                     'product')
         
         # maximum FDR
         self._parser.add_argument(
                 '--fdr', default=0.01, type=float,
-                help='FDR threshold to retain identifications '
-                     '(default: %(default)s)')
+                help='FDR threshold to accept identifications during the '
+                     'cascade search (default: %(default)s)')
 
         self._parser.add_argument(
                 '--fdr_tolerance_mass', default=0.1, type=float,
-                help='TODO')
+                help='mass difference bin width for the group FDR calculation '
+                     'during the second cascade level (default: %(default)s)')
         self._parser.add_argument(
                 '--fdr_tolerance_mode', default='Da', type=str,
                 choices=['Da', 'ppm'],
-                help='TODO')
+                help='mass difference bin unit for the group FDR calculation '
+                     'during the second cascade level (default: %(default)s)')
         self._parser.add_argument(
                 '--fdr_min_group_size', default=5, type=int,
-                help='TODO')
+                help='minimum group size for the group FDR calculation '
+                     'during the second cascade level (default: %(default)s)')
 
         # MODE
         # use an ANN index or the conventional brute-force mode
         self._parser.add_argument(
                 '--mode', default='ann', type=str, choices=['ann', 'bf'],
                 help="search using an approximate nearest neighbors or the "
-                     "conventional (brute-force) mode; 'bf': brute-force, "
-                     "'ann': approximate nearest neighbors indexing")
+                     "standard (brute-force) mode; 'bf': brute-force, "
+                     "'ann': approximate nearest neighbors")
 
         # bin size for the ANN index (Dalton)
         self._parser.add_argument(
@@ -150,13 +173,13 @@ class Config:
 
         # number of candidates to retrieve from the ANN index for each query
         self._parser.add_argument(
-                '--num_candidates', default=1000, type=int,
+                '--num_candidates', default=5000, type=int,
                 help='number of candidates to retrieve from the ANN index for '
                      'each query (default: %(default)s)')
 
         # minimum number of candidates for a query before ANN indexing is used
         self._parser.add_argument(
-                '--ann_cutoff', default=20000, type=int,
+                '--ann_cutoff', default=5000, type=int,
                 help='minimum number of candidates for a query before ANN '
                      'indexing is used to refine the candidates '
                      '(default: %(default)s)')
@@ -164,7 +187,7 @@ class Config:
         # custom Annoy parameters
         # number of ANN trees
         self._parser.add_argument(
-                '--num_trees', default=100, type=int,
+                '--num_trees', default=1000, type=int,
                 help='number of trees in the ANN index (default: %(default)s)')
 
         # number of nodes to explore during ANN searching
