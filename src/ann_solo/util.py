ec5d82250335be80b40f67cf11e2135d5fd113ea
Wout Bittremieux <wout@bittremieux.be>
2018-05-04 12:26:51 -0700

    PSM grouping for FDR based on distance to top-scored PSMs

--- src/ann_solo/util.py
+++ src/ann_solo/util.py
@@ -1,6 +1,6 @@
-import collections
 import itertools
 
+import numpy as np
 import pyteomics.auxiliary
 
 
@@ -33,51 +33,53 @@ def filter_group_fdr(psms, fdr=0.01, tol_mass=0, tol_mode=None,
                      min_group_size=5):
     """
     Filter PSMs exceeding the given FDR.
-    
-    PSMs are binned on their precursor mass difference prior to FDR filtering.
-    FDR filtering is applied separately to each common PSM bin and combined to
-    all uncommon PSM bins.
+
+    Prior to FDR filtering PSMs are grouped based on their precursor mass
+    difference. FDR filtering is applied separately to each common PSM group
+    and combined to all uncommon PSM groups.
 
     Args:
         psms: An iterable of `SpectrumMatch` PSMs to be filtered based on FDR.
         fdr: The minimum FDR threshold for filtering.
-        tol_mass: The bin width to bin PSMs. If None no binning is performed.
-        tol_mode: The unit in which the bin width is specified ('Da' or 'ppm').
-        If None no grouping is performed.
+        tol_mass: The mass range to group PSMs. If None no grouping is
+            performed.
+        tol_mode: The unit in which the mass range is specified ('Da' or
+            'ppm'). If None no grouping is performed.
         min_group_size: The minimum number of PSMs that should be present in
-            a bin for it to be considered common.
+            a group for it to be considered common.
 
     Returns:
         A generator of the `SpectrumMatch` PSMs with an FDR below the given FDR
         threshold. Each PSM is assigned its q-value in the `q` variable.
     """
-    if tol_mass is not None:
-        if tol_mode == 'Da':
-            _get_bin = lambda mass_diff, psm: int(mass_diff // tol_mass)
-        elif tol_mode == 'ppm':
-            _get_bin = (lambda mass_diff, psm:
-                        int((mass_diff / psm.exp_mass_to_charge * 10 ** 6)
-                            // tol_mass))
-        else:
-            _get_bin = lambda mass_diff, psm: -1
-    else:
-        _get_bin = lambda mass_diff, psm: -1
-    
-    mass_bins = collections.defaultdict(list)
-    mass_diffs = [(psm.exp_mass_to_charge - psm.calc_mass_to_charge)
-                  * psm.charge for psm in psms]
-    for mass_diff, psm in zip(mass_diffs, psms):
-        mass_bins[_get_bin(mass_diff, psm)].append(psm)
-    
+    psms_remaining = np.asarray(sorted(
+            psms, key=lambda psm: psm.search_engine_score, reverse=True))
+    exp_masses = np.asarray([psm.exp_mass_to_charge for psm in psms_remaining])
+    mass_diffs = np.asarray([(psm.exp_mass_to_charge - psm.calc_mass_to_charge)
+                             * psm.charge for psm in psms_remaining])
+
+    # start with the highest ranked PSM
     groups_common, groups_uncommon = [], []
-    for group in mass_bins.values():
-        if len(group) >= min_group_size:
-            groups_common.append(group)
+    while len(psms_remaining) > 0:
+        # find all remaining PSMs within the mass difference window
+        mass_diff_top = mass_diffs[0]
+        if tol_mass is None or tol_mode not in ('Da', 'ppm'):
+            mask = np.full(len(psms_remaining), True, dtype=bool)
+        elif tol_mode == 'Da':
+            mask = np.fabs(mass_diffs - mass_diffs[0]) <= tol_mass
+        elif tol_mode == 'ppm':
+            mask = (np.fabs(mass_diffs - mass_diffs[0]) / exp_masses * 10 ** 6 <= tol_mass)
+        if np.count_nonzero(mask) >= min_group_size:
+            groups_common.append(psms_remaining[mask])
         else:
-            groups_uncommon.extend(group)
-    
-    # calculate the FDR combined for all uncommon mass bins
-    # and separately for each common mass bin
+            groups_uncommon.extend(psms_remaining[mask])
+        # exclude the selected PSMs from further selections
+        psms_remaining = psms_remaining[~mask]
+        exp_masses = exp_masses[~mask]
+        mass_diffs = mass_diffs[~mask]
+        
+    # calculate the FDR combined for all uncommon mass difference groups
+    # and separately for each common mass difference group
     for psm in itertools.chain(
             filter_fdr(groups_uncommon, fdr),
             *[filter_fdr(group, fdr) for group in groups_common]):
