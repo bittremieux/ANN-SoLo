f3d02bfbb37ef5ad183e497f0ce9ea8803449350
Wout Bittremieux <wout@bittremieux.be>
2018-05-17 10:09:29 -0700

    Release 0.1.1

--- src/ann_solo/__init__.py
+++ src/ann_solo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0-1'
+__version__ = '0.1.1'
 
 __all__ = ['ann_solo', 'config', 'id_viewer', 'reader', 'spectral_library',
            'spectrum', 'spectrum_match', 'util', 'writer']
