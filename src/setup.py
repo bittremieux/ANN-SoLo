6adf98d9685d7ad5b37d6a04502e0574a1802fe3
Wout <wout@bittremieux.be>
2019-03-01 13:02:49 -0800

    Remove Python 3.5 tag

--- src/setup.py
+++ src/setup.py
@@ -63,7 +63,6 @@ setuptools.setup(
         'Programming Language :: Cython',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
