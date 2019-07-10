25d3cd25cc5143f6895c6519217ac887e69233d5
Wout Bittremieux <wout@bittremieux.be>
2019-07-09 21:38:46 -0700

    Update version number

--- src/ann_solo/__init__.py
+++ src/ann_solo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 __all__ = ['ann_solo', 'config', 'plot_ssm', 'reader', 'spectral_library',
            'spectrum', 'spectrum_match', 'utils', 'writer']
