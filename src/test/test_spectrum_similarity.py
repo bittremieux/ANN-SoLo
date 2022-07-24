e49b2aa718f73b64a34754ea9f50dc62be2506fb
issar <issar.arab@gmail.com>
2022-07-24 16:23:00 +0200

    Integrate UnitTest framework to the software

---
+++ src/test/test_spectrum_similarity.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestSpectrumSimilarityMethods(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_cosine(self):
+        pass
